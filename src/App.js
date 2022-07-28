cd462e632551632f11c78f9a4855d30b52884047
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-07-28 11:30:10 -0400

    updated sass colors throughout app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import SpeciesList from './components/speciesList/SpeciesList';
 
 function App() {
--- src/components/speciesList/SpeciesList.js
+++ src/components/speciesList/SpeciesList.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
+import SearchBar from '../searchBar/SearchBar';
 import SpeciesCard from '../speciesCard/SpeciesCard';
 import './SpeciesList.scss'
+import '../searchBar/SearchBar.scss'
 
 const SpeciesList = () => {
 
     //hooks
-    const [students, setStudents] = useState([])
+    const [species, setSpecies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
 
     //functions
     useEffect(() =>{
@@ -14,19 +17,41 @@ const SpeciesList = () => {
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                setStudents(data.students)
+                setSpecies(data.students)
             })
     },[])
+
+
+    // when search term is updated, this component will rerender
+    // what to do on a re-render?
+
+    let filteredSpecies = species;
+    
+    if(searchTerm){
+        filteredSpecies = species.filter(specie => {
+            const fullName = `${specie.firstName} ${specie.lastName}`;
+
+            const fullNameToLowerCase = fullName.toLowerCase();
+
+            const searchTermToLowerCase = searchTerm.toLowerCase();
+
+            return fullNameToLowerCase.includes(searchTermToLowerCase)
+        })
+    }
+
 
     //return or JSX
 
     return (
         <div className="speciesList">
-            {students.map(student => {
+            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+            {filteredSpecies.map(specie => {
                 return ( 
-                    <SpeciesCard student={student} />
+                    <SpeciesCard student={specie} />
                 )
             })}
+
+            {filteredSpecies.length == 0 && <div className="speciesList__noResults">No Results </div>}
         </div>
     )
 }
