092a7eda1dc7ee405f8046a3b8fe079d74fa5336
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-10-01 16:34:31 -0400

    added categories container with basic styling

---
+++ src/components/carousel/Categories.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import '../../pages/home/Home.scss'
+
+function Categories() {
+
+
+    return (
+        <div>
+            <div class="image-slider">
+                <div class="image-slider__item">
+                    <img src='https://via.placeholder.com/150x150' alt="" />
+                    <h4>Tropical Freshwater</h4>
+                </div>
+                <div class="image-slider__item">
+                    <img src='https://via.placeholder.com/150x150' alt="" />
+                    <h4>Marine Fish</h4>
+                </div>
+                <div class="image-slider__item">
+                    <img src='https://via.placeholder.com/150x150' alt="" />
+                    <h4>Betta</h4>
+                </div>
+                <div class="image-slider__item">
+                    <img src='https://via.placeholder.com/150x150' alt="" />
+                    <h4>Koi and Pong Fish</h4>
+                </div>
+                <div class="image-slider__item">
+                    <img src='https://via.placeholder.com/150x150' alt="" />
+                    <h4>Goldfish</h4>
+                </div>
+                <div class="image-slider__item">
+                    <img src='https://via.placeholder.com/150x150' alt="" />
+                    <h4>Bottom Feeders</h4>
+                </div>
+                <div class="image-slider__item">
+                    <img src='https://via.placeholder.com/150x150' alt="" />
+                    <h4>Marine invertebrates</h4>
+                </div>
+                <div class="image-slider__item">
+                    <img src='https://via.placeholder.com/150x150' alt="" />
+                    <h4>Discus</h4>
+                </div>
+            </div>  
+        </div>
+    )   
+}
+
+export default Categories
