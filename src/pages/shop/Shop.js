373059a95dff4b55996502622245c4069a896c12
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-10-17 01:53:25 -0400

    fix className on shop page

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -7,10 +7,10 @@ import FilterSection from '../../components/filterSection/FilterSection';
 function Shop() {
     return (
         <div className='shop-page'>
-            <div class="filter-section">
+            <div className="filter-section">
                 <FilterSection />
             </div>
-            <div class="product-grid">
+            <div className="product-grid">
                 <ProductsGrid />
             </div>
         </div>
