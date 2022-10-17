e670b0d1238334589cdf49f35db8e48cdf859798
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-10-16 23:54:38 -0400

    fix classname on product card, remove title decor

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -7,27 +7,27 @@ import './ProductCard.scss'
 
 function ProductCard() {
   return (
-    <div class="product-card">
-      <div class="product-card__canvas">
+    <div className="product-card">
+      <div className="product-card__canvas">
         <img src="https://via.placeholder.com/216x216" alt="" />
       </div>
-      <div class="product-card__content">
-        <div class="product-card__choices">
+      <div className="product-card__content">
+        <div className="product-card__choices">
           <p>
             <a href="#">More Choices available</a>
           </p>
         </div>
-        <div class="product-title-text">
-          <a class="product-card__title" href="#">
+        <div className="product-title-text">
+          <a className="product-card__title" href="#">
             <strong>Marineland</strong> Bio-Wheel Penguin Rite-Size C Filter
             Cartridge, 6 count
           </a>
         </div>
-        <div class="product-card__rating">
+        <div className="product-card__rating">
           {/* <FaRegStar /> (40) */}
         </div>
-        <div class="product-card__pricing">
-          <p class="product-card__message">FREE 1-3 day shipping over $49</p>
+        <div className="product-card__pricing">
+          <p className="product-card__message">FREE 1-3 day shipping over $49</p>
         </div>
       </div>
     </div>
