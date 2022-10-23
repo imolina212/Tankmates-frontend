e188beec962e5a78cee80f7fc36c94478a5aa49a
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-10-23 14:27:47 -0400

    productCard now fetching data from backend

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -1,33 +1,30 @@
 import React from "react";
 
 import './ProductCard.scss'
-// import FaStar from "react-icons/fa";
-// import FaStarHalfAlt from "react-icons/fa";
-// import FaRegStar from "react-icons/fa";
+import getStars from '../designUtils/getStars.js'
 
-function ProductCard() {
+function ProductCard({product}) {
+
   return (
     <div className="product-card">
       <div className="product-card__canvas">
-        <img src="https://via.placeholder.com/216x216" alt="" />
+        <img src="https://via.placeholder.com/260x260" alt="" />
       </div>
       <div className="product-card__content">
-        <div className="product-card__choices">
-          <p>
-            <a href="#">More Choices available</a>
-          </p>
+        <div className="product-card__content__choices">
+            <p>More Choices available</p>
         </div>
-        <div className="product-title-text">
-          <a className="product-card__title" href="#">
-            <strong>Marineland</strong> Bio-Wheel Penguin Rite-Size C Filter
-            Cartridge, 6 count
-          </a>
+        <div className="product-card__content__title">
+            <strong>{product.brand}</strong><span>{product.product_name}</span>
         </div>
-        <div className="product-card__rating">
-          {/* <FaRegStar /> (40) */}
+        <div className="product-card__content__rating">
+          {getStars(product.rating)}
         </div>
-        <div className="product-card__pricing">
-          <p className="product-card__message">FREE 1-3 day shipping over $49</p>
+        <div className="product-card__content__pricing">
+          ${product.price}
+        </div>
+        <div className="product-card__content__message">
+          <p>FREE 1-3 day shipping over $49</p>
         </div>
       </div>
     </div>
