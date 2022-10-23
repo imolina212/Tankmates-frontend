aef862a9607d6edcdc6af075cdfa62eda9a60c60
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-10-23 14:51:06 -0400

    made productGrid dynamic, fix grid alignment

--- src/components/productsGrid/ProductsGrid.js
+++ src/components/productsGrid/ProductsGrid.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import './ProductsGrid.scss'
 import ProductCard from '../productCard/ProductCard';
+import './ProductsGrid.scss';
+
+const API = process.env.REACT_APP_API_URL;
 
 function ProductsGrid() {
 
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=> {
+        axios.get(`${API}/products`)
+            .then((response) =>{
+                setProducts(response.data)
+            }).catch((err) => {
+                console.log(err);
+            })
+    }, [])
+
     return (
         <div className='product-grid'>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            {products.map((product, index) => {
+                return(
+                    <ProductCard key={index} product={product} />
+                )
+            })}
         </div>
     )
 }
