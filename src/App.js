b2478fecf83539da4b5cbb2c09662ba82dcc418b
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-10-25 22:26:40 -0400

    add cart page, add margin to app root scss

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import { Routes, Route } from "react-router-dom";
 
 import './App.scss';
 import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart"
 import Shop from "./pages/shop/Shop";
-import SpeciesDetailPage from './pages/SpeciesDetailPage';
-import Sidebar from "./components/sideBar/SideBar";
+import NavBar from "./components/navbar/NavBar.js"
+// import Sidebar from "./components/sideBar/SideBar";
+import ProductDetails from "./components/productDetails/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      {/* <NavBar /> */}
-      <Sidebar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/species/:id" element={<SpeciesDetailPage />} />
-      </Routes>
+      <NavBar />
+      {/* <Sidebar /> */}
+      <div className="page-content">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/shop/:id" element={<ProductDetails />} />
+        </Routes>
+      </div>
+      
     </div>
   );
 }
