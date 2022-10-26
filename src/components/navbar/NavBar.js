b379466628dd1545c497a27a292d3f37508939e2
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-10-25 22:47:35 -0400

    add cart icon to navbar, link tags to itemLinks

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,33 +1,44 @@
 import React, { useState } from 'react'
 
 import './NavBar.scss'
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { FiShoppingCart } from "react-icons/fi";
+import { useSelector } from 'react-redux';
 // import { GiHamburgerMenu } from "react-icons/gi";
 
 function NavBar(props) {
 
-    const [expandNavbar, setExpandNavbar] = useState(false);
+    // const [expandNavbar, setExpandNavbar] = useState(false);
 
-    const toggleMenu = () => {
-        console.log('navbar toggled')
-        setExpandNavbar(!expandNavbar)
-    }
+    // const toggleMenu = () => {
+    //     console.log('navbar toggled')
+    //     setExpandNavbar(!expandNavbar)
+    // }
+
+    const cart = useSelector((state) => state.cart)
+    const getTotalQuantity = () => {
+        let total = 0
+        cart.forEach(item => {
+          total += item.quantity
+        })
+        return total
+      }
     
     return (
-        <div class="navbar">
-            <div class="navbar__items">
-                <div class="navbar__logo">
+        <div className="navbar">
+            <div className="navbar__items">
+                <div className="navbar__logo">
                     <img src="https://via.placeholder.com/150" alt="TankMates logo" />
-                    <span><a href="/">Tankmates app</a></span>
+                    <span><Link to="/">Tankmates app</Link></span>
                 </div>
             
-                <div class="navbar__left">
-                    <ul class="navbar__itemLinks">
-                        <li><a href="/shop">Shop<span><MdKeyboardArrowDown /></span></a></li>
-                        <li>myTanks</li>
-                        <li>About</li>
-                        <li>Log In</li>
+                <div className="navbar__right">
+                    <ul className="navbar__itemLinks">
+                        <li><Link to="/shop">Shop<span><MdKeyboardArrowDown /></span></Link></li>
+                        <li>My Tanks</li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/cart" className="cart-icon"><FiShoppingCart /><p className='cart-totalItems'>{getTotalQuantity() || 0}</p></Link></li>
                     </ul>
                 </div>
             </div>
