8bde1c09d9a6f7c0d7e155c105b4d6e76276f0d0
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-11-03 16:42:39 -0400

    add media query for max-width 750px

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,21 +1,21 @@
-import React, { /*useState*/ } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import MenuItem from '../menuItem/MenuItem';
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { FiShoppingCart } from "react-icons/fi";
-// import { GiHamburgerMenu } from "react-icons/gi";
+import { GiHamburgerMenu } from "react-icons/gi";
 import './NavBar.scss'
 
 function NavBar(props) {
 
-    // const [expandNavbar, setExpandNavbar] = useState(false);
+    const [expandNavbar, setExpandNavbar] = useState(false);
 
-    // const toggleMenu = () => {
-    //     console.log('navbar toggled')
-    //     setExpandNavbar(!expandNavbar)
-    // }
+    const toggleMenu = () => {
+        console.log('navbar toggled')
+        setExpandNavbar(!expandNavbar)
+    }
 
     const cart = useSelector((state) => state.cart)
     const getTotalQuantity = () => {
@@ -27,26 +27,60 @@ function NavBar(props) {
       }
     
     return (
-        <div className="navbar">
-                <div className="navbar__logo">
-                    <div className="navbar__logo__image">
-                        <img src="/tankmates_logo2.png" alt="TankMates logo" />
-                    </div>
-                    <div className="navbar__logo__title">
-                        <Link to="/">tankmates</Link>
-                    </div>
-                </div>
-            
-                <div className="navbar__right">
-                    <ul className="navbar__right__itemLinks">
-                        <Link to="/shop"><MenuItem text="Shop" /></Link>
-                        <Link to="/tanks"><MenuItem text="My Tanks" /></Link>
-                        <Link to="/about"><MenuItem text="About" /></Link>
-                        <Link to="/cart"><MenuItem text={<><FiShoppingCart className="cart-icon" /><div className='cart-totalItems'>{getTotalQuantity() || 0}</div></>} /></Link>
-                    </ul>
-                </div>
+      <div className="navbar">
+        <div className="navbar__hamburgerMenu">
+          <GiHamburgerMenu onClick={toggleMenu} />
         </div>
-    )   
+        <div className="navbar__logo">
+          <div className="navbar__logo__image">
+            <img src="/tankmates_logo2.png" alt="TankMates logo" />
+          </div>
+          <div className="navbar__logo__title">
+            <Link to="/">tankmates</Link>
+          </div>
+        </div>
+
+        <div className="navbar__right">
+          <ul
+            className={
+              expandNavbar
+                ? "navbar__right__itemLinks"
+                : "navbar__right__collapsedItemLinks"
+            }
+          >
+            <Link to="/shop">
+              <MenuItem text="Shop" />
+            </Link>
+            <Link to="/tanks">
+              <MenuItem text="My Tanks" />
+            </Link>
+            <Link to="/about">
+              <MenuItem text="About" />
+            </Link>
+            <Link to="/cart">
+              <MenuItem
+                text={
+                  <>
+                    <FiShoppingCart className="cart-icon" />
+                    <div className="cart-totalItems">
+                      {getTotalQuantity() || 0}
+                    </div>
+                  </>
+                }
+              />
+            </Link>
+          </ul>
+          <div className="navbar__right__cart">
+            <Link to="/cart">
+              <FiShoppingCart className="navbar__right__cart__icon" />
+              <div className="navbar__right__cart__totalItems">
+                {getTotalQuantity() || 0}
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );   
 }
 
 export default NavBar;
