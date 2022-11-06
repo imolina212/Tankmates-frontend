3964140319c4937ba23fcc1be45d85b417b003e4
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-11-06 15:47:44 -0500

    add styling to Newsletter & footer item links

--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
+import { IoArrowForwardCircle } from 'react-icons/io5';
 import './Newsletter.scss'
 
 function Newsletter() {
     return (
         <div className="newsletterSubscription">
             <div className="newsletterSubscription__title">
-              <p>Subscribe to all the fun</p>
+              <h4>Subscribe to all the fun</h4>
               <p>Stay in touch with tankmates, see latest on updates, events and much more!</p>
             </div>
             <div>
               <form>
                 <div className="newsletterSubscription__formInput">
                   <input type="text" placeholder="Enter your email address"></input>
-                  <button>SIGN UP NOW</button>
+                  <button>SIGN UP NOW <IoArrowForwardCircle className="button-icon"/></button>
                 </div>
               </form>
             </div>
