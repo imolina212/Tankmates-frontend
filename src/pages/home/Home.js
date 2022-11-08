fd9bd932030026abc910fc5076330316372820e4
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-11-07 19:05:02 -0500

    remove newsletter card from home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,22 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import Carousel from '../../components/carousel/Carousel'
-import Categories from '../../components/carousel/Categories';
-import Newsletter from '../../components/newsletter/Newsletter';
-import './Home.scss'
+import Carousel from "../../components/carousel/Carousel";
+import Categories from "../../components/carousel/Categories";
+import "./Home.scss";
 
 export default function Home() {
-  return (
-    <div className='home-page'>
-      <div className="carousel-container">
-        <Carousel />
-      </div>
-      <div className="category-container">
-        <Categories />
-      </div>
-      <div className="emailNewsletter-container">
-        <Newsletter />
-      </div>
-    </div>
-  );
+	return (
+		<div className="home-page">
+			<div className="carousel-container">
+				<Carousel />
+			</div>
+			<div className="category-container">
+				<Categories />
+			</div>
+		</div>
+	);
 }
