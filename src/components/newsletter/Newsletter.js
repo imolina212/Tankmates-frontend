554de169cdb81ea68dd9efe908caf700e8cba534
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-11-15 20:23:58 -0500

    fix sign up button alignment

--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -1,25 +1,34 @@
-import React from 'react'
+import React from "react";
 
-import { IoArrowForwardCircle } from 'react-icons/io5';
-import './Newsletter.scss'
+import { IoArrowForwardCircle } from "react-icons/io5";
+import "./Newsletter.scss";
 
 function Newsletter() {
-    return (
-        <div className="newsletterSubscription">
-            <div className="newsletterSubscription__title">
-              <h4>Subscribe to all the fun</h4>
-              <p>Stay in touch with tankmates, see latest on updates, events and much more!</p>
-            </div>
-            <div>
-              <form onSubmit={(e)=> e.preventDefault()}>
-                <div className="newsletterSubscription__formInput">
-                  <input type="text" placeholder="Enter your email address"></input>
-                  <button>SIGN UP NOW <IoArrowForwardCircle className="button-icon"/></button>
-                </div>
-              </form>
-            </div>
-        </div>
-    )
+	return (
+		<div className="newsletterSubscription">
+			<div className="newsletterSubscription__title">
+				<h4>Subscribe to all the fun</h4>
+				<p>
+					Stay in touch with tankmates, see latest on updates, events
+					and much more!
+				</p>
+			</div>
+			<div>
+				<form onSubmit={(e) => e.preventDefault()}>
+					<div className="newsletterSubscription__formInput">
+						<input
+							type="text"
+							placeholder="Enter your email address"
+						></input>
+						<div className="button">
+							SIGN UP NOW
+							<IoArrowForwardCircle className="button-icon" />
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Newsletter;
