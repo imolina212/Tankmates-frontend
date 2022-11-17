5e89ebeda536ca4989f5c8d46942e66a1f369301
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-11-17 09:16:51 -0500

    Add signup api call to signup form

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,39 +1,93 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 
 import "./Signup.scss";
 
+const API = process.env.REACT_APP_API_URL;
+
 const Signup = () => {
+	const username = useRef("");
+	const firstname = useRef("");
+	const lastname = useRef("");
+	const email = useRef("");
+	const password = useRef("");
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const payload = {
+			username: username.current.value,
+			firstname: firstname.current.value,
+			lastname: lastname.current.value,
+			email: email.current.value,
+			password: password.current.value,
+		};
+		if (Object.values(payload).some((val) => !val)) return;
+		try {
+			console.log("signup.js payload", payload);
+			const response = await axios.post(`${API}/users/signup`, payload);
+			console.log("Signup.js response.data", response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className="container">
 			<div className="signup">
 				<div className="signup__heading">Create Account</div>
 				<div className="signup__form">
-					<form>
-						<label htmlFor="firstName" value="firstName"></label>
-						<input
-							type="text"
-							className="signup__form__inputField"
-							placeholder="First Name"
-						/>
-						<label htmlFor="lastName" value="lastName"></label>
-						<input
-							type="text"
-							className="signup__form__inputField"
-							placeholder="Last Name"
-						/>
-						<label htmlFor="email" value="email"></label>
-						<input
-							type="text"
-							className="signup__form__inputField"
-							placeholder="Email Address"
-						/>
-						<label htmlFor="password" value="password"></label>
-						<input
-							type="text"
-							className="signup__form__inputField"
-							placeholder="Password"
-						/>
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="username" value="username">
+							Username
+							<input
+								ref={username}
+								name="username"
+								type="text"
+								className="signup__form__inputField"
+								placeholder="Username"
+							/>
+						</label>
+						<label htmlFor="firstname" value="firstname">
+							First Name
+							<input
+								ref={firstname}
+								name="firstname"
+								type="text"
+								className="signup__form__inputField"
+								placeholder="First Name"
+							/>
+						</label>
+						<label htmlFor="lastname" value="lastname">
+							Last Name
+							<input
+								ref={lastname}
+								name="lastname"
+								type="text"
+								className="signup__form__inputField"
+								placeholder="Last Name"
+							/>
+						</label>
+						<label htmlFor="email" value="email">
+							Email Address
+							<input
+								ref={email}
+								name="email"
+								type="text"
+								className="signup__form__inputField"
+								placeholder="Email Address"
+							/>
+						</label>
+						<label htmlFor="password" value="password">
+							Password
+							<input
+								ref={password}
+								name="password"
+								type="text"
+								className="signup__form__inputField"
+								placeholder="Password"
+							/>
+						</label>
 						<input
 							type="submit"
 							className="signup__form__button"
