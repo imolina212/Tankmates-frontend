efe7610b85a5ae2b3e09bbad2074df729e94d651
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-11-16 22:04:15 -0500

    add password visibility toggle to login component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import axios from "axios";
+import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 import "./Login.scss";
 
 const API = process.env.REACT_APP_API_URL;
 
 const Login = ({ loggedInUserId, setUser }) => {
 	const [err, setErr] = useState("");
-	const inputRef = useRef(null);
+	const [showPassword, setShowPassword] = useState(false);
+	const password = useRef("");
+	const inputRef = useRef("");
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -19,16 +22,15 @@ const Login = ({ loggedInUserId, setUser }) => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+		if (!password.current.value || !inputRef.current.value) return;
 		try {
-			console.log(inputRef);
 			const response = await axios.post(`${API}/users/login`, {
 				email: inputRef.current.value,
+				password: password.current.value,
 			});
-			console.log("MY USER RESPONSE", response.data);
 			setUser(response.data.id);
 		} catch (err) {
 			if (err.response.status == 400 && err.response.data.err) {
-				console.log("if error");
 				setErr(err.response.data.err);
 			}
 			console.log(err);
@@ -36,10 +38,10 @@ const Login = ({ loggedInUserId, setUser }) => {
 	};
 
 	return (
-		<div className="login">
-			<div className="login__heading">Customer Log in</div>
-			<div className="login__form">
-				<form onSubmit={handleSubmit}>
+		<div className="container">
+			<div className="login">
+				<div className="login__heading">Customer Log in</div>
+				<form className="login__form" onSubmit={handleSubmit}>
 					<label htmlFor="email" value="email">
 						Email:<span style={{ color: "red" }}>*</span>
 						<div className="login__form__inputFieldWrapper">
@@ -50,21 +52,41 @@ const Login = ({ loggedInUserId, setUser }) => {
 							/>
 						</div>
 					</label>
+					<label htmlFor="standard-adornment-password">
+						Password
+					</label>
+					<input
+						type={showPassword ? "text" : "password"}
+						className="login__form__inputField"
+						ref={password}
+					/>
+					<div
+						className="login__visibility"
+						onClick={() => setShowPassword(!showPassword)}
+					>
+						{showPassword ? <MdVisibility /> : <MdVisibilityOff />}
+					</div>
 					<input
 						type="submit"
 						className="login__form__button"
-						value="Log in"
+						value="Sign in"
 					/>
 				</form>
-			</div>
-			<div className="login__message">
-				<div className="login__message__error">
-					{err && <span>{err}</span>}
-				</div>
-				<div className="login__message__terms">
-					By clicking "Log In", you agree to tankmates Terms of Use
-					and Privacy Policy. California residents can review our
-					Notice of Financial Incentives here.
+				<div className="login__message">
+					<div className="login__message__error">
+						{err && <span>{err}</span>}
+					</div>
+					<div className="login__message__terms">
+						By clicking "Sign In", you agree to tankmates Terms of
+						Use and Privacy Policy. California residents can review
+						our Notice of Financial Incentives here.
+					</div>
+					<div className="login__signupLink">
+						<p>
+							<span>New to tankmates? </span>
+							<Link to="/signup">Create account here</Link>
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
