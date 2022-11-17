34aac085794c15b87ae94fd0c9ea44693dbe0678
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-11-16 22:09:58 -0500

    add tankmates search, add / update species

--- src/components/speciesList/SpeciesList.js
+++ src/components/speciesList/SpeciesList.js
@@ -1,56 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import SearchBar from '../searchBar/SearchBar';
-import SpeciesCard from '../speciesCard/SpeciesCard';
-import './SpeciesList.scss'
-import '../searchBar/SearchBar'
+import axios from "axios";
+import "./SpeciesList.scss";
 
-const SpeciesList = () => {
+const API = process.env.REACT_APP_API_URL;
 
-    //hooks
-    const [species, setSpecies] = useState([])
-    const [searchTerm, setSearchTerm] = useState('')
-
-    //functions
-    useEffect(() =>{
-        const url = 'https://tankmates-backend.herokuapp.com/species'
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                // setSpecies(data.students)
-            })
-    },[])
-
-
-    let filteredSpecies = species;
-    
-    if(searchTerm){
-        filteredSpecies = species.filter(specie => {
-            const fullName = `${specie.firstName} ${specie.lastName}`;
-
-            const fullNameToLowerCase = fullName.toLowerCase();
-
-            const searchTermToLowerCase = searchTerm.toLowerCase();
-
-            return fullNameToLowerCase.includes(searchTermToLowerCase)
-        })
-    }
-
-
-    //return or JSX
-
-    return (
-        <div className="speciesList">
-            <SearchBar className="searchbar" searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-            {filteredSpecies.map((specie) => {
-                return ( 
-                    <SpeciesCard student={specie} key={specie.id} />
-                )
-            })}
-            {filteredSpecies.length === 0 && <div className="speciesList__noResults">No Results </div>}
-        </div>
-    )
-}
+const SpeciesList = ({ speciesList, tankId, handleClick }) => {
+	return (
+		<div className="speciesList">
+			{speciesList.map((specie, i) => (
+				<div className="speciesList__info" key={i}>
+					<div
+						className="speciesList__info__addSpecies"
+						onClick={() => {
+							handleClick(specie.id, tankId);
+						}}
+					>
+						+
+					</div>
+					<div className="speciesList__info__pic">
+						<img src={specie.pic} alt="" />
+					</div>
+					<div className="speciesList__info__name">
+						{specie.species_name}
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
 
 export default SpeciesList;
--- src/components/tankmates/Tankmates.js
+++ src/components/tankmates/Tankmates.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import axios from "axios";
 import { FiMinus, FiPlus } from "react-icons/fi";
+import SpeciesList from "../speciesList/SpeciesList";
 import "./Tankmates.scss";
 
 const API = process.env.REACT_APP_API_URL;
 
 const Tankmates = ({ tankId }) => {
 	const [tankmatesList, setTankmatesList] = useState([]);
+	const [tankmateSearch, setTankmateSearch] = useState([]);
+	const [speciesSearch, setSpeciesSearch] = useState("");
+	const [refresh, setRefresh] = useState(false);
 
 	useEffect(() => {
 		axios
@@ -18,35 +22,123 @@ const Tankmates = ({ tankId }) => {
 					response.data
 				);
 				setTankmatesList(response.data);
+				setRefresh(false);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
-	}, [tankId]);
+	}, [tankId, refresh]);
+
+	useEffect(() => {
+		if (speciesSearch) {
+			const searchFilters = tankmatesList
+				.map(({ species_id }) => species_id)
+				.join();
+			axios
+				.get(
+					`${API}/species/search/${speciesSearch}?filters=${searchFilters}`
+				)
+				.then((response) => {
+					console.log(
+						`Tankmates.js useEffect species/search/${speciesSearch} response.data\n`,
+						response.data
+					);
+					setTankmateSearch(response.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	}, [speciesSearch]);
+
+	const addNewSpecieToTank = async (speciesId, tankId) => {
+		try {
+			const response = await axios.post(
+				`${API}/tankmates/tank/${tankId}`,
+				{ speciesId }
+			);
+			console.log(
+				`SpeciesList.js handleClick speciesId=${speciesId} tankmates/tankmate/${tankId} response.data\n`,
+				response.data
+			);
+			setSpeciesSearch("");
+			setRefresh(true);
+			return response.data;
+			// setTankmateSearch(response.data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const updateQuantity = async (speciesId, removeTankmateId = 0) => {
+		console.log("updateQuatinignit", speciesId, removeTankmateId);
+		try {
+			let response;
+			if (!removeTankmateId) {
+				response = await addNewSpecieToTank(speciesId, tankId);
+				console.log("added new species", response);
+				return;
+			} else {
+				response = await axios.delete(
+					`${API}/tankmates/${removeTankmateId}`
+				);
+				console.log("removed a tankmate", response);
+			}
+			setRefresh(true);
+			return;
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
 	return (
 		<div className="tankmates">
-			Tankmates component
-			{tankmatesList.map((tankmate, i) => (
-				<div className="tankmates__info" key={i}>
-					<div className="tankmates__info__pic">
-						<img src={tankmate.pic} alt="" />
-					</div>
-					<div className="tankmates__info__name">
-						{tankmate.species_name}
-					</div>
-					<div className="tankmates__info__quantity">
-						<div className="button">
-							<FiMinus />
+			<input
+				type="search"
+				placeholder="add new species to tank"
+				value={speciesSearch}
+				onChange={(e) => setSpeciesSearch(e.target.value)}
+			/>
+			{tankmateSearch && speciesSearch && (
+				<SpeciesList
+					speciesList={tankmateSearch}
+					tankId={tankId}
+					handleClick={addNewSpecieToTank}
+				/>
+			)}
+			{tankmatesList.map(
+				({ species_id, pic, species_name, tankmate_ids }, i) => (
+					<div className="tankmates__info" key={i}>
+						<div className="tankmates__info__pic">
+							<img src={pic} alt="" />
 						</div>
-						{tankmate.quantity}
-						<div className="button">
-							<FiPlus />
+						<div className="tankmates__info__name">
+							{species_name}
 						</div>
+						<div className="tankmates__info__quantity">
+							<div
+								className="button"
+								onClick={() =>
+									updateQuantity(
+										species_id,
+										tankmate_ids.pop()
+									)
+								}
+							>
+								<FiMinus />
+							</div>
+							{tankmate_ids.length}
+							<div
+								className="button"
+								onClick={() => updateQuantity(species_id)}
+							>
+								<FiPlus />
+							</div>
+						</div>
+						{/* <div className="tankmates__status">{tankmate.status}</div> */}
 					</div>
-					{/* <div className="tankmates__status">{tankmate.status}</div> */}
-				</div>
-			))}
+				)
+			)}
 		</div>
 	);
 };
