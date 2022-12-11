79c2a2dac404847fc7ae54b1c313a29d5300e0fa
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-12-11 12:09:14 -0500

    add sticky product header component w/styling

---
+++ src/components/stickyProductHeader/StickyProductHeader.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import QuantityPicker from "../quantityPicker/QuantityPicker";
+import Button from "../button/Button";
+import "./StickyProductHeader.scss";
+
+const StickyProductHeader = () => {
+	const [bgColor, setBgColor] = useState(false);
+
+	const changeBgColorOnScroll = () => {
+		const y = window.scrollY;
+
+		if (y >= 400) {
+			setBgColor(true);
+		} else {
+			setBgColor(false);
+		}
+	};
+
+	window.addEventListener("scroll", changeBgColorOnScroll);
+
+	return (
+		<div
+			className={
+				bgColor
+					? "sticky-product-header show"
+					: "sticky-product-header hide"
+			}
+		>
+			<div className="sticky-product-header__image">
+				<img src="https://via.placeholder.com/80" />
+			</div>
+			<div className="sticky-product-header__details">
+				<div className="sticky-product-header__details__title">
+					Aquarium Co-Op Auto Feeder
+				</div>
+				<div className="sticky-product-header__details__price">
+					$24.99
+				</div>
+			</div>
+			<QuantityPicker />
+			<Button name="Add to Cart" variant="primary" size="sq large" />
+		</div>
+	);
+};
+
+export default StickyProductHeader;
