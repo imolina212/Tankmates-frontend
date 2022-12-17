a8d71ae2d9c1476e17c151b68f609c480b187c49
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-12-17 13:25:10 -0500

    add horizontal padding to stars from getStars

--- src/components/designUtils/getStars.js
+++ src/components/designUtils/getStars.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+import "./_colors.scss";
 
 const getStars = (rating) => {
 	let starsToAdd = rating;
@@ -9,16 +10,16 @@ const getStars = (rating) => {
 	while (ratingStars.length < 5) {
 		if (starsToAdd >= 1) {
 			ratingStars.push(
-				<span className="star">
-					<BsStarFill />
+				<span className="star" style={{ padding: "0px 1.5px" }}>
+					<BsStarFill style={{ color: "#ff9800" }} />
 				</span>
 			);
 			starsToAdd--;
 		}
 		if (starsToAdd > 0 && starsToAdd < 1) {
 			ratingStars.push(
-				<span className="star">
-					<BsStarHalf />
+				<span className="star" style={{ padding: "0px 1.5px" }}>
+					<BsStarHalf style={{ color: "#ff9800" }} />
 				</span>
 			);
 			starsToAdd = 0;
@@ -26,8 +27,8 @@ const getStars = (rating) => {
 
 		if (starsToAdd === 0 && ratingStars.length < 5) {
 			ratingStars.push(
-				<span className="star">
-					<BsStar />
+				<span className="star" style={{ padding: "0px 1.5px" }}>
+					<BsStar style={{ color: "#ff9800" }} />
 				</span>
 			);
 		}
