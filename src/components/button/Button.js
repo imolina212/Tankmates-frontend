59906c66c1ae7cec8239af9d770f6a59b83af089
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-12-22 16:44:39 -0500

    add optional arrow icon to btn component

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,18 +1,21 @@
 import React from "react";
 
+import { IoArrowForwardCircle } from "react-icons/io5";
 import "./Button.scss";
 
 const Button = ({
 	name = "name",
 	variant = "variant",
 	size = "size",
+	arrow = false,
 	...buttonProps
 }) => {
 	return (
-		<div className="button-container">
-			<button {...buttonProps} className={`btn ${variant} ${size}`}>
-				{name}
-			</button>
+		<div {...buttonProps} className={`btn ${variant} ${size}`}>
+			{name}
+			{arrow && (
+				<IoArrowForwardCircle className="btn__arrow-icon" size={20} />
+			)}
 		</div>
 	);
 };
