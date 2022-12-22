e5dd5b4d2364e422f912d4faede636ff94ed7892
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-12-22 18:57:37 -0500

    add btn component to total.js, remove old btn css

--- src/components/cart/Total.js
+++ src/components/cart/Total.js
@@ -1,45 +1,47 @@
-import './Total.scss'
-import { useSelector, useDispatch } from 'react-redux'
-import { clearCart } from '../../redux/cartSlice'
+import { useSelector, useDispatch } from "react-redux";
+import { clearCart } from "../../redux/cartSlice";
+import Button from "./../button/Button";
+import "./Total.scss";
 
 function Total() {
+	const cart = useSelector((state) => state.cart);
+	const dispatch = useDispatch();
 
-  const cart = useSelector((state) => state.cart)
-  const dispatch = useDispatch();
+	const getTotal = () => {
+		let totalQuantity = 0;
+		let totalPrice = 0;
+		cart.forEach((item) => {
+			totalQuantity += item.quantity;
+			totalPrice += item.price * item.quantity;
+		});
+		return { totalPrice, totalQuantity };
+	};
 
-
-  const getTotal = () => {
-    let totalQuantity = 0
-    let totalPrice = 0
-    cart.forEach(item => {
-      totalQuantity += item.quantity
-      totalPrice += item.price * item.quantity
-    })
-    return {totalPrice, totalQuantity}
-  }
-  
-  return (
-    <div className="total">
-      <h2>ORDER SUMMARY</h2>
-      <div>
-        <p className="total__p">
-          total ({getTotal().totalQuantity} items) 
-          : <strong>${getTotal().totalPrice}</strong>
-        </p>
-      </div>
-      <div className="total__buttonStack">
-        <button
-          className="button clearCart"
-          onClick={() => dispatch(clearCart())}
-        > Clear Cart
-        </button>
-        <button
-          className="button checkout"
-        > Proceed to Checkout
-        </button>
-      </div>
-    </div>
-  )
+	return (
+		<div className="total">
+			<h2>ORDER SUMMARY</h2>
+			<div>
+				<p className="total__p">
+					total ({getTotal().totalQuantity} items) :{" "}
+					<strong>${getTotal().totalPrice}</strong>
+				</p>
+			</div>
+			<div className="total__buttonStack">
+				<Button
+					name="Proceed to Checkout"
+					variant="primary"
+					size="sq medium extend"
+					arrow={true}
+				/>
+				<Button
+					name="Clear Cart"
+					variant="outline-danger"
+					size="sq medium extend"
+					onClick={() => dispatch(clearCart())}
+				/>
+			</div>
+		</div>
+	);
 }
 
-export default Total
+export default Total;
