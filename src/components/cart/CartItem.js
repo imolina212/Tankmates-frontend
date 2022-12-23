5e27632a866fbc5025e11eedfa208ef133aec06f
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-12-22 19:19:32 -0500

    change cart item remove btn to 'x' logo

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -5,6 +5,7 @@ import {
 } from "../../redux/cartSlice";
 import { useDispatch } from "react-redux";
 import QuantityPicker from "./../quantityPicker/QuantityPicker";
+import { MdClear } from "react-icons/md";
 import "./CartItem.scss";
 
 function CartItem({ id, image, title, price, quantity = 0 }) {
@@ -12,28 +13,28 @@ function CartItem({ id, image, title, price, quantity = 0 }) {
 
 	return (
 		<div className="cartItem">
-			<img className="cartItem__image" src={image} alt="item" />
-			<div className="cartItem__info">
-				<p className="cartItem__title">{title}</p>
-				<p className="cartItem__price">
-					<small>$</small>
-					<strong>{price}</strong>
-				</p>
-				<div className="cartItem__quantityPicker">
-					<QuantityPicker
-						label="Qty :"
-						quantity={quantity}
-						onIncrement={() => dispatch(incrementQuantity(id))}
-						onDecrement={() => dispatch(decrementQuantity(id))}
-					/>
+			<>
+				<img className="cartItem__image" src={image} alt="item" />
+				<div className="cartItem__info">
+					<p className="cartItem__title">{title}</p>
+					<p className="cartItem__price">
+						<small>$</small>
+						<strong>{price}</strong>
+					</p>
+					<div className="cartItem__quantityPicker">
+						<QuantityPicker
+							label="Qty :"
+							quantity={quantity}
+							onIncrement={() => dispatch(incrementQuantity(id))}
+							onDecrement={() => dispatch(decrementQuantity(id))}
+						/>
+					</div>
 				</div>
-				<button
-					className="cartItem__removeButton"
-					onClick={() => dispatch(removeItem(id))}
-				>
-					Remove
-				</button>
-			</div>
+			</>
+			<MdClear
+				className="cartItem__removeButton"
+				onClick={() => dispatch(removeItem(id))}
+			/>
 		</div>
 	);
 }
