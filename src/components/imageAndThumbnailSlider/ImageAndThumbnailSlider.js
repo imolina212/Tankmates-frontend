ba3459566eef79c85a358428776df81dd450cf66
imolina212 <86175612+imolina212@users.noreply.github.com>
2023-01-03 13:46:17 -0500

    fix alignment of selectedImg &  collection < 4

--- src/components/imageAndThumbnailSlider/ImageAndThumbnailSlider.js
+++ src/components/imageAndThumbnailSlider/ImageAndThumbnailSlider.js
@@ -3,16 +3,7 @@ import React, { useState } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import "./ImageAndThumbnailSlider.scss";
 
-const ImageAndThumbnailSlider = ({ id }) => {
-	let imageArray = [
-		{ id: 0, src: "/productImages/promo1_1.webp" },
-		{ id: 1, src: "/productImages/promo2_1.jpg" },
-		{ id: 2, src: "/productImages/promo3.jpg" },
-		{ id: 3, src: "https://via.placeholder.com/150" },
-		{ id: 4, src: "https://via.placeholder.com/150" },
-		{ id: 5, src: "https://via.placeholder.com/150" },
-		{ id: 6, src: "https://via.placeholder.com/150" },
-	];
+const ImageAndThumbnailSlider = ({ id, pic: imageArray }) => {
 	const [selectedImg, setSelectedImg] = useState(0);
 	const [hover, setHover] = useState(false);
 
@@ -23,15 +14,17 @@ const ImageAndThumbnailSlider = ({ id }) => {
 	return (
 		<div className="imageSlider">
 			<div className="imageSlider__selected">
-				<div className="imageSlider__selected__item">
+				<div
+					className="imageSlider__selected__item"
+					onMouseEnter={() => setHover(true)}
+					onMouseLeave={() => setHover(false)}
+				>
 					<div
 						className={
 							hover
 								? "imageSlider__selected__left"
 								: "imageSlider__selected__left hide"
 						}
-						onMouseEnter={() => setHover(true)}
-						onMouseLeave={() => setHover(false)}
 						onClick={() => {
 							selectedImg > 0
 								? setSelectedImg(selectedImg - 1)
@@ -40,20 +33,13 @@ const ImageAndThumbnailSlider = ({ id }) => {
 					>
 						<IoIosArrowBack className="imageSlider__selected__btn" />
 					</div>
-					<img
-						src={imageArray[selectedImg].src}
-						onMouseEnter={() => setHover(true)}
-						onMouseLeave={() => setHover(false)}
-						alt=""
-					/>
+					<img src={imageArray[selectedImg]} alt="" />
 					<div
 						className={
 							hover
 								? "imageSlider__selected__right"
 								: "imageSlider__selected__right hide"
 						}
-						onMouseEnter={() => setHover(true)}
-						onMouseLeave={() => setHover(false)}
 						onClick={() => {
 							selectedImg < imageArray.length - 1
 								? setSelectedImg(selectedImg + 1)
@@ -67,16 +53,19 @@ const ImageAndThumbnailSlider = ({ id }) => {
 			<div className="imageSlider__collection">
 				{imageArray.map((image, i) => {
 					return (
-						<img
+						<div
 							className={
-								imageArray[selectedImg].id === i
+								selectedImg === i
 									? "imageSlider__collection__item imageSlider__collection__item__selected"
 									: "imageSlider__collection__item"
 							}
-							src={image.src}
-							alt=""
-							onClick={() => handleClick(i)}
-						/>
+						>
+							<img
+								src={image}
+								alt=""
+								onClick={() => handleClick(i)}
+							/>
+						</div>
 					);
 				})}
 			</div>
