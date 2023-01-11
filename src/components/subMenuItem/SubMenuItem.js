063fdb3f4c67f104b7d019ed505deb7122a91482
imolina212 <86175612+imolina212@users.noreply.github.com>
2023-01-11 16:39:42 -0500

    add submenu item component

---
+++ src/components/subMenuItem/SubMenuItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./SubMenuItem.scss";
+
+const SubMenuItem = ({ text }) => {
+	return <li className="sub-menu-item">{text}</li>;
+};
+
+export default SubMenuItem;
