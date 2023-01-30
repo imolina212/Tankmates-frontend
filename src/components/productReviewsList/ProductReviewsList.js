36a85c6d8fce1f595abd34cddd218175758edb50
isidro <isidromolina@pursuit.org>
2023-01-29 21:38:32 -0500

    ProductReviewsList now rendering fetched data

--- src/components/productReviewsList/ProductReviewsList.js
+++ src/components/productReviewsList/ProductReviewsList.js
@@ -1,110 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import getStars from "../designUtils/getStars";
 import { IoPersonOutline } from "react-icons/io5";
 import "./ProductReviewsList.scss";
 
+const URL = process.env.REACT_APP_API_URL;
+
 const ProductReviewsList = () => {
+	const [productReviews, setProductReviews] = useState([]);
+	const { id } = useParams();
+
+	useEffect(() => {
+		axios
+			.get(`${URL}/product-reviews/${id}`)
+			.then((response) => {
+				setProductReviews(response.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [URL, id]);
+
 	return (
-		<div className="productReviewsList">
-			<div className="productReviewsList__item">
-				<div className="productReviewsList__item__rating">
-					<span className="productReviewsList__item__rating__stars">
-						{getStars(5)}
-					</span>
-					<span className="productReviewsList__item__rating__timestamp">
-						08/27/22
-					</span>
+		<div>
+			{productReviews.length ? (
+				productReviews.map((review) => {
+					console.log("review -->", review);
+					const { content, date_created, rating, title, username } =
+						review;
+					return (
+						<div className="productReviewsList__item">
+							<div className="productReviewsList__item__rating">
+								<span className="productReviewsList__item__rating__stars">
+									{getStars(rating)}
+								</span>
+								<span className="productReviewsList__item__rating__timestamp">
+									{new Date(date_created).toLocaleDateString(
+										"en-us"
+									)}
+								</span>
+							</div>
+							<div className="productReviewsList__item__profile">
+								<div className="productReviewsList__item__profile__pic-container">
+									<IoPersonOutline className="pic" />
+								</div>
+								<div className="productReviewsList__item__profile__name">
+									{username}
+								</div>
+								<div className="productReviewsList__item__profile__location">
+									San Fransisco, CA, US
+								</div>
+							</div>
+							<div className="productReviewsList__item__content">
+								<div className="productReviewsList__item__content__title">
+									{title}
+								</div>
+								<div className="productReviewsList__item__body">
+									<p>{content}</p>
+								</div>
+							</div>
+						</div>
+					);
+				})
+			) : (
+				<div className="productReviewsList--empty">
+					Be the first to leave a review!
 				</div>
-				<div className="productReviewsList__item__profile">
-					<div className="productReviewsList__item__profile__pic-container">
-						<IoPersonOutline className="pic" />
-					</div>
-					<div className="productReviewsList__item__profile__name">
-						Emmanuel A.
-					</div>
-					<div className="productReviewsList__item__profile__location">
-						San Fransisco, CA, US
-					</div>
-				</div>
-				<div className="productReviewsList__item__content">
-					<div className="productReviewsList__item__content__title">
-						Excellent carnivore treat
-					</div>
-					<div className="productReviewsList__item__body">
-						<p>
-							I am always pleased with the food, plants and other
-							products I order from you. I like how you guys
-							package the plants and products. Thanks so much!
-						</p>
-					</div>
-				</div>
-			</div>
-			<div className="productReviewsList__item">
-				<div className="productReviewsList__item__rating">
-					<span className="productReviewsList__item__rating__stars">
-						{getStars(5)}
-					</span>
-					<span className="productReviewsList__item__rating__timestamp">
-						12/11/22
-					</span>
-				</div>
-				<div className="productReviewsList__item__profile">
-					<div className="productReviewsList__item__profile__pic-container">
-						<IoPersonOutline className="pic" />
-					</div>
-					<div className="productReviewsList__item__profile__name">
-						John C.
-					</div>
-					<div className="productReviewsList__item__profile__location">
-						Punta Gorda, FL, US
-					</div>
-				</div>
-				<div className="productReviewsList__item__content">
-					<div className="productReviewsList__item__content__title">
-						Less waste,less cost,win win
-					</div>
-					<div className="productReviewsList__item__body">
-						<p>
-							These pellets are a great alternative when I am not
-							able to feed live foods!
-						</p>
-					</div>
-				</div>
-			</div>
-			<div className="productReviewsList__item">
-				<div className="productReviewsList__item__rating">
-					<span className="productReviewsList__item__rating__stars">
-						{getStars(5)}
-					</span>
-					<span className="productReviewsList__item__rating__timestamp">
-						10/17/22
-					</span>
-				</div>
-				<div className="productReviewsList__item__profile">
-					<div className="productReviewsList__item__profile__pic-container">
-						<IoPersonOutline className="pic" />
-					</div>
-					<div className="productReviewsList__item__profile__name">
-						Dennis M.
-					</div>
-					<div className="productReviewsList__item__profile__location">
-						San Fransisco, CA, US
-					</div>
-				</div>
-				<div className="productReviewsList__item__content">
-					<div className="productReviewsList__item__content__title">
-						Great daily food.
-					</div>
-					<div className="productReviewsList__item__body">
-						<p>
-							I have been feeding my oscar cichlid this food as
-							his primary diet for over 5 years and I still have a
-							beautiful young vibrant fish.
-						</p>
-					</div>
-				</div>
-			</div>
+			)}
 			<div className="productReviewsList__paginate">
 				<div className="productReviewsList__paginate__btn">
 					Load More
