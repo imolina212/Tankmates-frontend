22ff904fedd6672b8137ecc3e41024e88fd2b82c
isidro <isidromolina@pursuit.org>
2023-02-04 16:57:36 -0500

    fix productDetail missing child key prop warning

--- src/components/designUtils/getStars.js
+++ src/components/designUtils/getStars.js
@@ -6,31 +6,35 @@ import "./_colors.scss";
 const getStars = (rating) => {
 	let starsToAdd = rating;
 	let ratingStars = [];
+	let i = 0;
 
 	while (ratingStars.length < 5) {
 		if (starsToAdd >= 1) {
 			ratingStars.push(
-				<span className="star" style={{ padding: "0px 1.5px" }}>
+				<span key={i} className="star" style={{ padding: "0px 1.5px" }}>
 					<BsStarFill style={{ color: "#ff9800" }} />
 				</span>
 			);
 			starsToAdd--;
+			i++;
 		}
 		if (starsToAdd > 0 && starsToAdd < 1) {
 			ratingStars.push(
-				<span className="star" style={{ padding: "0px 1.5px" }}>
+				<span key={i} className="star" style={{ padding: "0px 1.5px" }}>
 					<BsStarHalf style={{ color: "#ff9800" }} />
 				</span>
 			);
 			starsToAdd = 0;
+			i++;
 		}
 
 		if (starsToAdd === 0 && ratingStars.length < 5) {
 			ratingStars.push(
-				<span className="star" style={{ padding: "0px 1.5px" }}>
+				<span key={i} className="star" style={{ padding: "0px 1.5px" }}>
 					<BsStar style={{ color: "#ff9800" }} />
 				</span>
 			);
+			i++;
 		}
 	}
 	return ratingStars;
