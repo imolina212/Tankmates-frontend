cf36b01e46a7923240cb36275d3429471c29534d
isidro <isidromolina@pursuit.org>
2023-02-27 01:45:33 -0500

    connect newsletter to MailChimp API endpoint

--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
+import axios from "axios";
 import Button from "./../button//Button";
+import "./../button/Button.scss";
 import "./Newsletter.scss";
 
+const API = process.env.REACT_APP_API_URL;
+
 function Newsletter() {
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			await axios.post(`${API}/newsletter-signup`, {
+				firstName,
+				lastName,
+				email,
+			});
+			setFirstName("");
+			setLastName("");
+			setEmail("");
+
+			if (!firstName || !lastName || !email) {
+				window.alert("unsuccessful");
+			} else {
+				window.alert("successful");
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<div className="newsletterSubscription">
 			<div className="newsletterSubscription__title">
@@ -14,19 +45,47 @@ function Newsletter() {
 				</p>
 			</div>
 			<div>
-				<form onSubmit={(e) => e.preventDefault()}>
-					<div className="newsletterSubscription__formInput">
+				<form
+					className="newsletterSubscription__formInput"
+					onSubmit={handleSubmit}
+				>
+					<div className="newsletterSubscription__formInput__name">
 						<input
 							type="text"
-							placeholder="Enter your email address"
-						></input>
-						<Button
-							name="sign up now"
-							variant="primary"
-							size="sq large"
-							arrow={true}
-						></Button>
+							name="firstName"
+							id="first-name"
+							value={firstName} // needs to be 3 or more than
+							onChange={(e) => setFirstName(e.target.value)}
+							placeholder="First Name"
+						/>
+
+						<input
+							type="text"
+							name="lastName"
+							id="last-name"
+							value={lastName} // needs to be 3 or more than
+							onChange={(e) => setLastName(e.target.value)}
+							placeholder="Last Name"
+						/>
 					</div>
+
+					<input
+						className="newsletterSubscription__formInput__email"
+						type="email"
+						name="email"
+						id="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						placeholder="Email"
+					/>
+
+					<button
+						className="btn primary rd sq small extend"
+						variant="primary"
+						type="submit"
+					>
+						SIGN UP NOW
+					</button>
 				</form>
 			</div>
 		</div>
