6bb1827b4f18f8e8d24efb1155daf564d6b27631
isidro <isidromolina@pursuit.org>
2023-05-06 16:27:06 -0400

    migrate Button component to TS

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,6 +3,14 @@ import React from "react";
 import { IoArrowForwardCircle } from "react-icons/io5";
 import "./Button.scss";
 
+interface ButtonInterface {
+	name: string;
+	arrow: boolean;
+	variant: string;
+	size: string;
+	in_stock: boolean;
+}
+
 const Button = ({
 	name = "name",
 	variant = "variant",
@@ -10,7 +18,7 @@ const Button = ({
 	arrow = false,
 	in_stock,
 	...buttonProps
-}) => {
+}: ButtonInterface) => {
 	return (
 		<button {...buttonProps} className={`btn ${variant} ${size}`}>
 			{name}
